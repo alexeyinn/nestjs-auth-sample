6e88b6138b7eebe1738d112e1d1404d6cd8e10dd
alexeyinn <alexey.i.nn@gmail.com>
2022-07-12 11:30:02 +0300

    feat: user register with at and rt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("/local/signup")
-  sighupLocal() {}
+  sighupLocal(@Body() dto: AuthDto) {
+    return this.authService.sighupLocal(dto);
+  }
 
   @Post("/local/signin")
   sighinLocal() {}
